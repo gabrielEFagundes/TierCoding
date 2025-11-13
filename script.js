d1a169486c2efacec99b126c9a9ad2e1d1bcbb01
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-11-13 16:48:08 -0300

    :sparkles: feat: Finishing the essencial

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const darkmodeBtn = document.getElementById('darkmode');
+const draggable = document.querySelectorAll('.draggable');
+const droppable = document.querySelectorAll('.droppable');
+const dragZone = document.querySelectorAll('.dragzone');
 
 darkmodeBtn.addEventListener('click', () => {
     document.body.classList.toggle('background-darkmode');
@@ -10,4 +13,35 @@ darkmodeBtn.addEventListener('click', () => {
     for(let section of sections){
         section.classList.toggle('cardSection-darkmode');
     }
+})
+
+draggable.forEach(element => {
+    element.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('text/plain', event.target.id);
+        element.classList.add('dragging');
+    })
+})
+
+let removedChild = null;
+draggable.forEach(element => {
+    element.addEventListener('dragend', () => {
+       element.classList.remove('dragging');
+    })
+})
+
+droppable.forEach(element => {
+    element.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    })
+
+    element.addEventListener('drop', (event) => {
+        event.preventDefault();
+        
+        const data = event.dataTransfer.getData('text/plain');
+        const el = document.getElementById(data);
+
+        if(el){
+            element.appendChild(el);
+        }
+    })
 })
